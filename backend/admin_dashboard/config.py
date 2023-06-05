f24deb9b58de6b12852dbd81a92ca2164c144320
kundlatsch <gustavo.kundlatsch@gmail.com>
2023-06-05 02:05:36 -0300

    Remove .env config needed

--- backend/admin_dashboard/config.py
+++ backend/admin_dashboard/config.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "My_very_secret_key"
 DEBUG = True
